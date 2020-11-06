7a2784ee85b49790e6ef1ac177c5f7ff945ac58d
swfz <sawafuji.09@gmail.com>
2020-11-06 15:23:22 +0900

    chore: 不要箇所の削除

--- google/scheduler-run/cloud_run.tf
+++ google/scheduler-run/cloud_run.tf
@@ -1,4 +1,3 @@
-
 resource "google_cloud_run_service" "default" {
   name     = "cloudrun-srv"
   location = local.region
@@ -47,25 +46,6 @@ resource "google_cloud_run_service_iam_policy" "run_policy" {
   service = google_cloud_run_service.default.name
   policy_data = data.google_iam_policy.run_act_as.policy_data
 }
-
-// resource "google_cloud_run_service_iam_binding" "binding" {
-//   location = google_cloud_run_service.default.location
-//   project = google_cloud_run_service.default.project
-//   service = google_cloud_run_service.default.name
-//   role = "roles/iam.serviceaccounts.actAs"
-//   members = [
-//     "servi${local.project_number}-compute@developer.gserviceaccount.com",
-//   ]
-// }
-
-
-// resource "google_cloud_run_service_iam_member" "run_invoker" {
-//   project  = google_cloud_run_service.default.project
-//   location = google_cloud_run_service.default.location
-//   service  = google_cloud_run_service.default.name
-//   role     = "roles/run.invoker"
-//   member   = "serviceAccount:${google_service_account.run_invoker.email}"
-// }
 
 locals {
   url = google_cloud_run_service.default.status[0].url
